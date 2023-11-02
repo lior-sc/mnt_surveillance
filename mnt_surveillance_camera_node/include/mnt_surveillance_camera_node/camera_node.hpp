baafbd06e3ff9e3165b26f1f26a659f1e198e50a
lior-sc <liorsmail89@gmail.com>
2023-11-02 15:18:04 +0200

    add varaible declaration to camera node

--- mnt_surveillance_camera_node/include/mnt_surveillance_camera_node/camera_node.hpp
+++ mnt_surveillance_camera_node/include/mnt_surveillance_camera_node/camera_node.hpp
@@ -1,6 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
+#include "mnt_surveillance_camera_node/cameras/camera.hpp"
+#include "mnt_surveillance_camera_node/cameras/camera_encoded.hpp"
 #include "mnt_surveillance_camera_node/cameras/webcam.hpp"
+#include "mnt_surveillance_camera_node/cameras/webcam_encoded.hpp"  
 #include "mnt_surveillance_camera_node/cameras/random_noise.hpp"
+#include "mnt_surveillance_camera_node/cameras/fixed_frame_encoded.hpp"
 
 namespace mnt_surveillance
 {
@@ -12,12 +16,14 @@ namespace mnt_surveillance
             CameraNode();
 
         private:
+            void declare_parameters();
             void add_cameras();
             void run();
             void publish_video_streams();
             rclcpp::Node::SharedPtr node_handle_;
             rclcpp::TimerBase::SharedPtr publish_timer_;
             std::list<camera::Camera *> cameras_;
+            std::list<camera::CameraEncoded *> encoded_cameras_;
 
             // Other member functions here
         };
--- mnt_surveillance_camera_node/src/camera_node.cpp
+++ mnt_surveillance_camera_node/src/camera_node.cpp
@@ -7,19 +7,34 @@ CameraNode::CameraNode() : Node("camera_node")
     RCLCPP_INFO(this->get_logger(), "Init surveillance camera node");
     // get current node handle
     node_handle_ = rclcpp::Node::SharedPtr(this);
+    declare_parameters();
     add_cameras();
     run();
+}
+
+void CameraNode::declare_parameters()
+{
+    // change this to accept from yaml configuration file
+    node_handle_->declare_parameter<int>("frame_width_px", 50);
+    node_handle_->declare_parameter<int>("frame_height_px", 50);
+    node_handle_->declare_parameter<int>("frame_rate", 30);
 }
 
 void CameraNode::add_cameras()
 {
     // change this to accept from yaml configuration file
-    cameras_.push_back(new camera::Webcam(
-        node_handle_,
-        "hello1"));
+    // cameras_.push_back(new camera::Webcam(
+    //     node_handle_,
+    //     "camera/webcam"));
     cameras_.push_back(new camera::RandomNoise(
         node_handle_,
-        "hello2"));
+        "camera/random_noise"));
+    // encoded_cameras_.push_back(new camera::WebcamEncoded(
+    //     node_handle_,
+    //     "webcam_encoded"));
+    encoded_cameras_.push_back(new camera::FixedFrameEncoded(
+        node_handle_,
+        "fixed_frame_encoded"));
 }
 
 void CameraNode::run()
@@ -35,6 +50,13 @@ void CameraNode::publish_video_streams()
     for (const auto &camera : cameras_)
     {
         camera->publish();
+    }
+
+    for (const auto &camera : encoded_cameras_)
+    {
+        camera->publish_capture();
+        camera->publish_encoded_data();
+        camera->publish_decoded_image();
     }
 }
 
