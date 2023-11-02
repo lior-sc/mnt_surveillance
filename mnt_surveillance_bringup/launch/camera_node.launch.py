df9e0838c8ca9af8e1ecd567c9eb3d9d638b5013
lior-sc <liorsmail89@gmail.com>
2023-11-02 19:08:45 +0200

    create launch and param files

---
+++ mnt_surveillance_bringup/launch/camera_node.launch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Author: Lior Schwartz
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    camera_node_param_dir = LaunchConfiguration(
+        'camera_node_param_dir',
+        default=os.path.join(
+            get_package_share_directory('mnt_surveillance_bringup'),
+            'param',
+            'camera_node_params' + '.yaml'))
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'camera_node_param_dir',
+            default_value=camera_node_param_dir,
+            description='Full path to camera_node parameter file to load'),
+
+        Node(
+            package='mnt_surveillance_camera_node',
+            executable='camera_node',
+            parameters=[camera_node_param_dir],
+            output='screen')
+    ])
---
+++ mnt_surveillance_bringup/launch/master_node.launch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Author: Lior Schwartz
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    master_node_param_dir = LaunchConfiguration(
+        'master_node_param_dir',
+        default=os.path.join(
+            get_package_share_directory('mnt_surveillance_bringup'),
+            'param',
+            'master_node_params' + '.yaml'))
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'master_node_param_dir',
+            default_value=master_node_param_dir,
+            description='Full path to master_node parameter file to load'),
+
+        Node(
+            package='mnt_surveillance_master_node',
+            executable='master_node',
+            parameters=[master_node_param_dir],
+            output='screen')
+    ])
