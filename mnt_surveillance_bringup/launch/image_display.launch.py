660b48a24acf941a09390118df200d39b317870c
lior-sc <liorsmail89@gmail.com>
2023-11-03 21:12:58 +0200

    modify rqt_image_display launch file. still not working

--- mnt_surveillance_bringup/launch/image_display.launch.py
+++ mnt_surveillance_bringup/launch/image_display.launch.py
@@ -6,8 +6,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'image_topic',
+            default_value='/video/decoded_data',
+            description='Image topic to display'
+        ),
+
         Node(
             package='rqt_image_view',
             executable='rqt_image_view',
-            output='screen')
+            name='rqt_image_view',
+            parameters=[{'image': LaunchConfiguration('image_topic')}]
+        )
     ])
